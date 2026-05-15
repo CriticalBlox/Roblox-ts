24fa56778cb9f317f5348e8f1a9825cfd7c90324
Kilian <kvanovertvelt@gmail.com>
2026-05-15 20:19:12 +0200

    feat: config player + spawn

---
+++ src/server/config_player.server.ts
@@ -0,0 +1,12 @@
+import { Players } from "@rbxts/services";
+
+Players.PlayerAdded.Connect((player) => {
+  player.Team = undefined
+
+  player.CameraMode = Enum.CameraMode.LockFirstPerson
+
+  player.CharacterAdded.Connect((character) => {
+    const humanoid = character.WaitForChild("Humanoid") as Humanoid;
+    humanoid.WalkSpeed = 40;
+  });
+});
