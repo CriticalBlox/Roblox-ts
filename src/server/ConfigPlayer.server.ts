7f074cde775fc3098edd3fa8282675a651410241
Kilian <kvanovertvelt@gmail.com>
2026-05-16 19:16:03 +0200

    feat: heath Ui

--- src/server/ConfigPlayer.server.ts
+++ src/server/ConfigPlayer.server.ts
@@ -8,5 +8,7 @@ Players.PlayerAdded.Connect((player) => {
   player.CharacterAdded.Connect((character) => {
     const humanoid = character.WaitForChild("Humanoid") as Humanoid;
     humanoid.WalkSpeed = 40;
+    humanoid.MaxHealth = 200;
+    humanoid.Health = 200;
   });
 });
